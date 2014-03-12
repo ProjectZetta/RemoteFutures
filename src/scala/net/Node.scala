dfe156af01d00eb45f2ea5d9186dfd2ca43c23c7
Marvin Hansen <marvin.hansen@gmail.com>
2014-03-12 14:53:13 +0100

    Added dummy classes for compiling proposed API withing components

---
+++ src/scala/net/Node.scala
@@ -0,0 +1,14 @@
+/* Copyright (c) 2014 Marvin Hansen.
+ * www.marvin-hansen.tel.
+ * ALl RIGHTS RESERVED
+ ***************************
+ * Project: DistributedRemoteFutures
+ * User: Marvin Hansen
+ * Web: www.marvin-hansen.tel
+ * Date: 3/12/14 (/dd/mm/yy)
+ * Time: 1:22 PM (CET)
+ */
+package net
+
+/* Dummy class for API draft  */
+sealed case class Node(name: String)
---
+++ src/scala/net/NodeSelector.scala
@@ -0,0 +1,15 @@
+/* Copyright (c) 2014 Marvin Hansen.
+ * www.marvin-hansen.tel.
+ * ALl RIGHTS RESERVED
+ ***************************
+ * Project: DistributedRemoteFutures
+ * User: Marvin Hansen
+ * Web: www.marvin-hansen.tel
+ * Date: 3/12/14 (/dd/mm/yy)
+ * Time: 2:03 PM (CET)
+ */
+package net
+
+/**
+  */
+class NodeSelector {}
