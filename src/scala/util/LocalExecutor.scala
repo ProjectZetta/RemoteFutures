3fe6de1e66fcfbd3666ec8674ae863cc3e58d537
Marvin Hansen <marvin.hansen@gmail.com>
2014-03-12 14:55:29 +0100

    Refactored LocalExecutor in separate object for isolating Hazelcast dependency while preserving  compatibility with existing sample code.

---
+++ src/scala/util/LocalExecutor.scala
@@ -0,0 +1,47 @@
+/* Copyright (c) 2014 Marvin Hansen.
+ * www.marvin-hansen.tel.
+ * ALl RIGHTS RESERVED
+ ***************************
+ * Project: DistributedRemoteFutures
+ * User: Marvin Hansen
+ * Web: www.marvin-hansen.tel
+ * Date: 3/12/14 (/dd/mm/yy)
+ * Time: 1:02 PM (CET)
+ */
+package util
+
+import java.net.InetAddress
+import scala.concurrent.duration.Duration
+import java.util.concurrent.TimeUnit
+import core.{DistributionStrategy, RemoteConfig}
+import com.hazelcast.core.{Hazelcast, HazelcastInstance, IExecutorService}
+import com.hazelcast.config.{Config, ExecutorConfig}
+
+/**
+  */
+object LocalExecutor {
+
+  object Implicits {
+    implicit val LocalConfig: RemoteConfig = {
+      val host = InetAddress.getLocalHost
+      val poolSize = 4
+      //  Thread Pool size on Remote host
+      val dist = DistributionStrategy.LOAD_BALANCING
+      val T = Duration(2, TimeUnit.SECONDS)
+      val config: RemoteConfig = RemoteConfig(host, T, poolSize, dist)
+      config
+    }
+
+    implicit val LocalExecution: IExecutorService = {
+      //http://www.hazelcast.org/docs/latest/manual/html-single/hazelcast-documentation.html#distributed-executor-service
+      val execConf = new ExecutorConfig().setName("RemoteExecutor").setPoolSize(LocalConfig.threadPoolSize)
+      val conf: Config = new Config().addExecutorConfig(execConf)
+      val h: HazelcastInstance = Hazelcast.newHazelcastInstance(conf)
+      //create & return remote executor
+      val executor = h.getExecutorService("my-distributed-executor")
+      executor
+    }
+
+  }
+
+}
