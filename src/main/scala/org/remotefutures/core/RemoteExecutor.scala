a9117e576e5bc4e30bc77366a93d1ec082619e37
Marvin Hansen <marvin.hansen@gmail.com>
2014-03-13 17:59:45 +0100

    Fixed glitch during last merge that has messed up RemoteExecutor file

--- src/main/scala/org/remotefutures/core/RemoteExecutor.scala
+++ src/main/scala/org/remotefutures/core/RemoteExecutor.scala
@@ -4,30 +4,15 @@
 package org.remotefutures.core
 
 
-//Suggesting an interface  that extends Executor, for instance:
-//import java.util.concurrent.Executor
-
-//trait RemoteExecutor extends Executor
-// with an companion oject, just like the one below.
-
-//Then, the actual implementation could be something like
-//class akkaRemoteExecutor extends RemoteExecutor {}
-
-// so that the companion object is used to switch implementation,
-//according to whatever criterion.
-
-
-
 object RemoteExecutor {
-  def apply(config : Config) : RemoteExecutor = {
-    def createInstance[T](fqn: String) : T = {
-      Class.forName( fqn ).newInstance().asInstanceOf[T]
+  def apply(config: Config): RemoteExecutor = {
+    def createInstance[T](fqn: String): T = {
+      Class.forName(fqn).newInstance().asInstanceOf[T]
     }
 
-    createInstance[RemoteExecutor]( config.remoteExecutorClassname )
+    createInstance[RemoteExecutor](config.remoteExecutorClassname)
   }
 }
-
 
 
 /**
@@ -37,15 +22,8 @@ object RemoteExecutor {
  * of nodes determined by certain properties through a node-selector.
  *
  */
-<<<<<<< HEAD
-object RemoteExecutor {
-  def fromConfig(config: RemoteConfig): RemoteExecutor = {
-    new RemoteExecutor
-  }
-=======
 trait RemoteExecutor {
   def execute[C, T](body: () => T, bodyContext: C): Unit
->>>>>>> 9044e427f902483f38cb126dc8304f935794172f
 }
 
 /**
