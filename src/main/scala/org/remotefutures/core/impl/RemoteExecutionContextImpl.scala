d731c30db984ea75c8576b795f676b68fce3bd12
Marvin Hansen <marvin.hansen@gmail.com>
2014-03-13 16:11:39 +0100

    Added factory for creating a RemoteExecutor in RemoteExecutionContextImpl
    Essentially, this materializes the need for a proper interface
    of RemoteExecutor.

--- src/main/scala/org/remotefutures/core/impl/RemoteExecutionContextImpl.scala
+++ src/main/scala/org/remotefutures/core/impl/RemoteExecutionContextImpl.scala
@@ -4,8 +4,26 @@ import org.remotefutures.core.{RemoteExecutionContext, RemoteExecutor}
 
 private[core] class RemoteExecutionContextImpl private[impl] (res: RemoteExecutor, reporter: Throwable => Unit) extends RemoteExecutionContext {
 
+
+  val executor: RemoteExecutor = res match {
+    case null => createRemoteExecutorService
+    case some => some
+  }
+
+  /**
+   * Facility to create a RemoteExecutor used in the context
+   * in case none is given. The actual point is providing
+   * some kind of near-zero overhead default RemoteExecutor
+   *
+   * @return RemoteExecutor
+   */
+  def createRemoteExecutorService: RemoteExecutor = ???
+
+
   override def execute[C, T](body: () => T, bodyContext: C): Unit = {
-    // here comes invocation of res
+    // call should be something like executor.execute(body,bodyContext)
+    // This goes hand in hand with the interface definition of RemoteExecutor
+    // already suggested.
   }
 
   override def reportFailure(t: Throwable) = reporter(t)
