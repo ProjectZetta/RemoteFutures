cd503b779a7f177738c49961ff963df9d9ed82ae
Martin Senne <martin.senne@inubit.com>
2014-03-20 16:09:56 +0100

    Moved all to Build.scala instead of build.sbt.

--- project/Build.scala
+++ project/Build.scala
@@ -25,9 +25,6 @@ object Resolvers {
   val maven2Resolver = "Java.net Maven2 Repository" at "http://download.java.net/maven/2/"
   val mavenCentralResolver = "Maven central" at "http://repo1.maven.org/maven2/"
 
-  // resolvers += Resolver.sonatypeRepo("snapshots"),
-  // val SonatypeSnapshots = "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
-
   val allResolvers = Seq (sonatypeReleases, typesafeResolver, compassResolver,twitterResolver, maven2Resolver, mavenCentralResolver)
 }
 
@@ -59,18 +56,44 @@ object MyBuild extends Build {
   lazy val root: Project = Project(
     "root",
     file("."),
-    settings = buildSettings
+    settings = buildSettings ++ SbtMultiJvm.multiJvmSettings
       ++ Seq (resolvers := allResolvers)
       ++ Seq(
-      libraryDependencies <+= (scalaVersion)("org.scala-lang" % "scala-reflect" % _),
-      libraryDependencies ++= (
-        if (scalaVersion.value.startsWith("2.10")) List("org.scalamacros" % "quasiquotes" % "2.0.0-M3" cross CrossVersion.full)
-        else Nil
+        libraryDependencies <+= (scalaVersion)("org.scala-lang" % "scala-reflect" % _),
+        libraryDependencies ++= (
+          if (scalaVersion.value.startsWith("2.10")) List("org.scalamacros" % "quasiquotes" % "2.0.0-M3" cross CrossVersion.full)
+          else Nil
         ))
       ++ Seq (libraryDependencies ++= allDeps )
-      // ++ Seq( run <<= run in Compile in core )
-  )
+      ++ Seq (libraryDependencies ++= Seq(
+        "com.typesafe.akka" %% "akka-remote" % akkaVersion,
+        "com.typesafe.akka" %% "akka-multi-node-testkit" % akkaVersion
+      ))
+      ++ Seq(
+      // make sure that MultiJvm test are compiled by the default test compilation
+      compile in MultiJvm <<= (compile in MultiJvm) triggeredBy (compile in Test),
+      // disable parallel tests
+      parallelExecution in Test := false,
+      // make sure that MultiJvm tests are executed by the default test target,
+      // and combine the results from ordinary test and multi-jvm tests
+      executeTests in Test <<= (executeTests in Test, executeTests in MultiJvm) map {
+        case (testResults, multiNodeResults) =>
+          val overall =
+            if (testResults.overall.id < multiNodeResults.overall.id)
+              multiNodeResults.overall
+            else
+              testResults.overall
+          Tests.Output(overall,
+            testResults.events ++ multiNodeResults.events,
+            testResults.summaries ++ multiNodeResults.summaries)
+      })
+  ) configs (MultiJvm)
+
+
+  // ++ Seq( run <<= run in Compile in core )
   // aggregate(macros, core)
+
+
 
 //  lazy val macros: Project = Project(
 //    "macros",
@@ -92,35 +115,35 @@ object MyBuild extends Build {
 
 
 
-  val project = Project(
-    id = "multi-node-scala",
-    base = file("multinode."),
-    settings = Project.defaultSettings ++ SbtMultiJvm.multiJvmSettings ++ Seq(
-      name := "multi-node-scala",
-      version := "1.0",
-      //scalaVersion := "2.10.3",
-      libraryDependencies ++= Seq(
-        "com.typesafe.akka" %% "akka-remote" % akkaVersion,
-        "com.typesafe.akka" %% "akka-multi-node-testkit" % akkaVersion
-      ),
-        // "org.scalatest" %% "scalatest" % "2.0" % "test"),
-      // make sure that MultiJvm test are compiled by the default test compilation
-      compile in MultiJvm <<= (compile in MultiJvm) triggeredBy (compile in Test),
-      // disable parallel tests
-      parallelExecution in Test := false,
-      // make sure that MultiJvm tests are executed by the default test target,
-      // and combine the results from ordinary test and multi-jvm tests
-      executeTests in Test <<= (executeTests in Test, executeTests in MultiJvm) map {
-        case (testResults, multiNodeResults) =>
-          val overall =
-            if (testResults.overall.id < multiNodeResults.overall.id)
-              multiNodeResults.overall
-            else
-              testResults.overall
-          Tests.Output(overall,
-            testResults.events ++ multiNodeResults.events,
-            testResults.summaries ++ multiNodeResults.summaries)
-      }
-    )
-  ) configs (MultiJvm)
+//  val project = Project(
+//    id = "multi-node-scala",
+//    base = file("multinode."),
+//    settings = Project.defaultSettings ++ SbtMultiJvm.multiJvmSettings ++ Seq(
+//      name := "multi-node-scala",
+//      version := "1.0",
+//      //scalaVersion := "2.10.3",
+//      libraryDependencies ++= Seq(
+//        "com.typesafe.akka" %% "akka-remote" % akkaVersion,
+//        "com.typesafe.akka" %% "akka-multi-node-testkit" % akkaVersion
+//      ),
+//        // "org.scalatest" %% "scalatest" % "2.0" % "test"),
+//      // make sure that MultiJvm test are compiled by the default test compilation
+//      compile in MultiJvm <<= (compile in MultiJvm) triggeredBy (compile in Test),
+//      // disable parallel tests
+//      parallelExecution in Test := false,
+//      // make sure that MultiJvm tests are executed by the default test target,
+//      // and combine the results from ordinary test and multi-jvm tests
+//      executeTests in Test <<= (executeTests in Test, executeTests in MultiJvm) map {
+//        case (testResults, multiNodeResults) =>
+//          val overall =
+//            if (testResults.overall.id < multiNodeResults.overall.id)
+//              multiNodeResults.overall
+//            else
+//              testResults.overall
+//          Tests.Output(overall,
+//            testResults.events ++ multiNodeResults.events,
+//            testResults.summaries ++ multiNodeResults.summaries)
+//      }
+//    )
+//  ) configs (MultiJvm)
 }
