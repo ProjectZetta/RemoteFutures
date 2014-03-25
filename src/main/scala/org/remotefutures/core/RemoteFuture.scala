158a13285724a1ae3c43d52a217795b023539941
Marvin Hansen <marvin.hansen@gmail.com>
2014-03-25 14:20:31 +0100

    added inheritance to Runnable

--- src/main/scala/org/remotefutures/core/RemoteFuture.scala
+++ src/main/scala/org/remotefutures/core/RemoteFuture.scala
@@ -1,13 +1,4 @@
-/* Copyright (c) 2014 Marvin Hansen.
- * www.marvin-hansen.tel.
- * ALl RIGHTS RESERVED
- ***************************
- * Project: DistributedRemoteFutures
- * User: Marvin Hansen
- * Web: www.marvin-hansen.tel
- * Date: 3/25/14 (/dd/mm/yy)
- * Time: 2:07 PM (CET)
- */
+
 package org.remotefutures.core
 
 import scala.concurrent.Future
