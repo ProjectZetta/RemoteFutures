7125312e370f64838eec1268dbd6000758ca8063
Martin Senne <mail@martin-senne.de>
2014-03-26 18:58:40 +0100

    New concept for monad stuff and RemoteFuture.

---
+++ src/main/scala/org/remotefutures/examples/DocumentationExample.scala
@@ -0,0 +1,33 @@
+package org.remotefutures.examples
+
+import org.remotefutures.core.RemoteFuture
+import scala.concurrent.Future
+
+/**
+ * Created by mse on 26.03.14.
+ */
+object DocumentationExample {
+  def main(args: Array[String]) : Unit = {
+
+    def lengthyComputation1 : Int = {
+      // calculate an extremly complicated Int value
+      42
+    }
+
+    val x : Future[Int] = RemoteFuture {
+      lengthyComputation1
+    }
+
+    def lengthyComputation2 : (Int => Int) = {
+      // calculate fancy stuff
+      x => x * 2
+    }
+
+    // y contains the result of lengthyComputation2( lengthyComputation1 )
+    val y: Future[Int] = x map lengthyComputation2
+
+    val y2 = RemoteFuture {
+      lengthyComputation2( lengthyComputation1 )
+    }
+  }
+}
