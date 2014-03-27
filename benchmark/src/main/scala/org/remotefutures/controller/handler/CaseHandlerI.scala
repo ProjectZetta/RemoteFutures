8b3cb6833c6607b2a9032797d06e7523753c220f
Marvin Hansen <marvin.hansen@gmail.com>
2014-03-27 20:22:07 +0100

    Migrated CaseHandler Interface and Benchmark

---
+++ benchmark/src/main/scala/org/remotefutures/controller/handler/CaseHandlerI.scala
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2014 Marvin Hansen.
+*/
+package org.remotefutures.controller.handler
+
+import org.remotefutures.model.{FeatureWeights, CaseMap}
+import org.remotefutures.controller.manager.CaseManager
+
+/**
+ * @author Marvin Hansen
+ */
+
+object CaseHandler extends CaseHandler
+
+abstract class CaseHandler
+
+trait CaseHandlerI {
+  def calcMostSimilarCases(refCases: CaseMap, cm: CaseManager, w: FeatureWeights): Unit
+}
+
+
