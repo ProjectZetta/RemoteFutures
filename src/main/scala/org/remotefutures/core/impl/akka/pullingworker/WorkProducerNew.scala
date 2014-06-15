3d80e82f6ee3c4fc0c4c7386defbb533c217c372
Martin Senne <mail@martin-senne.de>
2014-06-15 23:42:46 +0200

    First part of wiring with pulling workers.

---
+++ src/main/scala/org/remotefutures/core/impl/akka/pullingworker/WorkProducerNew.scala
@@ -0,0 +1,52 @@
+package org.remotefutures.core.impl.akka.pullingworker.old
+
+import java.util.UUID
+
+import akka.actor.{Actor, ActorLogging, ActorRef}
+import org.remotefutures.core.impl.akka.pullingworker.{Frontend, Work}
+
+import scala.concurrent.duration._
+import scala.concurrent.forkjoin.ThreadLocalRandom
+
+object WorkProducer {
+  case object Tick
+}
+
+class WorkProducer(frontend: ActorRef) extends Actor with ActorLogging {
+  import context.dispatcher
+  import org.remotefutures.core.impl.akka.pullingworker.old.WorkProducer._
+
+  def scheduler = context.system.scheduler
+  def rnd = ThreadLocalRandom.current
+  def nextWorkId(): String = UUID.randomUUID().toString
+
+  var n = 0
+
+  override def preStart(): Unit =
+  // waiting five whopping seconds before sending a tick???.
+  // Is there a particular reason for using this strategy?
+    scheduler.scheduleOnce(5.seconds, self, Tick)
+
+  // override postRestart so we don't call preStart and schedule a new Tick
+  override def postRestart(reason: Throwable): Unit = ()
+
+  def receive = {
+    case Tick =>
+      n += 1
+      log.info("Produced work: {}", n)
+      val work = Work(nextWorkId(), n)
+      frontend ! work
+      context.become(waitAccepted(work), discardOld = false)
+
+  }
+
+  def waitAccepted(work: Work): Actor.Receive = {
+    case Frontend.Ok =>
+      context.unbecome()
+      scheduler.scheduleOnce(rnd.nextInt(3, 10).seconds, self, Tick)
+    case Frontend.NotOk =>
+      log.info("Work not accepted, retry after a while")
+      scheduler.scheduleOnce(3.seconds, frontend, work)
+  }
+
+}
---
+++ src/multi-jvm/scala/org/remotefutures/pingpong/MultiNodeSample.scala
@@ -0,0 +1,52 @@
+package org.remotefutures.pingpong
+
+import akka.remote.testkit.MultiNodeSpec
+import akka.testkit.ImplicitSender
+import akka.actor.{ Props, Actor }
+
+// start with "multi-jvm:test-only org.remotefutures.pingpong.MultiNodeSampleSpec"
+
+class MultiNodeSampleSpecMultiJvmNode1 extends MultiNodeSample
+class MultiNodeSampleSpecMultiJvmNode2 extends MultiNodeSample
+
+object MultiNodeSample {
+  class Ponger extends Actor {
+    def receive = {
+      case "ping" => sender() ! "pong"
+    }
+  }
+}
+
+class MultiNodeSample extends MultiNodeSpec(MultiNodeSampleConfig)
+with STMultiNodeSpec with ImplicitSender {
+
+  import MultiNodeSampleConfig._
+  import MultiNodeSample._
+
+  def initialParticipants = roles.size
+
+  "A MultiNodeSample" must {
+
+    "wait for all nodes to enter a barrier" in {
+      enterBarrier("startup")
+    }
+
+    "send to and receive from a remote node" in {
+      runOn(node1) {
+        enterBarrier("deployed")
+        println("Sending ping to ponger")
+
+        val ponger = system.actorSelection(node(node2) / "user" / "ponger")
+        ponger ! "ping"
+        expectMsg("pong")
+      }
+
+      runOn(node2) {
+        system.actorOf(Props[Ponger], "ponger")
+        enterBarrier("deployed")
+      }
+
+      enterBarrier("finished")
+    }
+  }
+}
