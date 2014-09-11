1698ca3071183fbfde6701baef0fc452c6def3d1
Martin Senne <mail@martin-senne.de>
2014-09-11 12:54:25 +0200

    build settings and dependencies adapted for runner

--- project/Build.scala
+++ project/Build.scala
@@ -1,12 +1,13 @@
 import sbt.Keys._
 import sbt._
+import spray.revolver.RevolverPlugin.Revolver
 
 
 object BuildSettings {
   val buildSettings = Defaults.defaultSettings ++ Seq(
     organization := "org.remotefutures",
     version := "0.0.1",
-    scalaVersion := "2.11.1",
+    scalaVersion := "2.11.2",
 
     scalacOptions ++= Seq(
       "-deprecation",
@@ -36,34 +37,49 @@ object Resolvers {
 }
 
 object Dependencies {
-  val scalaTestVersion = "2.2.0"
+  val scalaTestVersion  = "2.2.0"
   val scalaCheckVersion = "1.11.4"
-  val akkaVersion = "2.3.4"
-  val disruptorVersion = "3.2.1"
+  val akkaVersion       = "2.3.5"
+  val disruptorVersion  = "3.2.1"
+  val sprayVersion      = "1.3.1"
 
-  val scalaAsync = "org.scala-lang.modules" %% "scala-async" % "0.9.1"
+  val scalaAsync       = "org.scala-lang.modules" %% "scala-async" % "0.9.1"
 
-  val scalaCheck = "org.scalacheck" %% "scalacheck" % scalaCheckVersion withSources() withJavadoc()
-  val scalaTest = "org.scalatest" %% "scalatest" % scalaTestVersion % "test" withSources() withJavadoc()
+  val scalaCheck       = "org.scalacheck" %% "scalacheck" % scalaCheckVersion withSources() withJavadoc()
+  val scalaTest        = "org.scalatest" %% "scalatest" % scalaTestVersion % "test" withSources() withJavadoc()
 
-  val config = "com.typesafe" % "config" % "1.2.0" withSources() withJavadoc()
+  val config           = "com.typesafe" % "config" % "1.2.0" withSources() withJavadoc()
 
-  val akkaActor = "com.typesafe.akka" %% "akka-actor" % akkaVersion withSources() withJavadoc()
-  val akkaTestkit = "com.typesafe.akka" %% "akka-testkit" % akkaVersion withSources() withJavadoc()
-  val akkaCluster = "com.typesafe.akka" %% "akka-cluster" % akkaVersion withSources() withJavadoc()
-  val akkaRemote = "com.typesafe.akka" %% "akka-remote" % akkaVersion withSources() withJavadoc()
-  val akkaMultiNode = "com.typesafe.akka" %% "akka-multi-node-testkit" % akkaVersion withSources() withJavadoc()
-  val akkaContrib = "com.typesafe.akka" %% "akka-contrib" % akkaVersion withSources() withJavadoc()
+  val akkaActor        = "com.typesafe.akka" %% "akka-actor" % akkaVersion withSources() withJavadoc()
+  val akkaTestkit      = "com.typesafe.akka" %% "akka-testkit" % akkaVersion withSources() withJavadoc()
+  val akkaCluster      = "com.typesafe.akka" %% "akka-cluster" % akkaVersion withSources() withJavadoc()
+  val akkaRemote       = "com.typesafe.akka" %% "akka-remote" % akkaVersion withSources() withJavadoc()
+  val akkaMultiNode    = "com.typesafe.akka" %% "akka-multi-node-testkit" % akkaVersion withSources() withJavadoc()
+  val akkaContrib      = "com.typesafe.akka" %% "akka-contrib" % akkaVersion withSources() withJavadoc()
 
   // benchmark dependencies
-  val jxl = "net.sourceforge.jexcelapi" % "jxl" % "2.6.12" withSources() withJavadoc()
-  val xstream = "com.thoughtworks.xstream" % "xstream" % "1.4.4" withSources() withJavadoc()
-  val commons = "commons-lang" % "commons-lang" % "2.6" withSources() withJavadoc()
-  val comCol = "org.apache.commons" % "commons-collections4" % "4.0" withSources() withJavadoc()
-  val json = "net.minidev" % "json-smart" % "1.0.9" withSources() withJavadoc()
+  val jxl              = "net.sourceforge.jexcelapi" % "jxl" % "2.6.12" withSources() withJavadoc()
+  val xstream          = "com.thoughtworks.xstream" % "xstream" % "1.4.4" withSources() withJavadoc()
+  val commons          = "commons-lang" % "commons-lang" % "2.6" withSources() withJavadoc()
+  val comCol           = "org.apache.commons" % "commons-collections4" % "4.0" withSources() withJavadoc()
+  val json             = "net.minidev" % "json-smart" % "1.0.9" withSources() withJavadoc()
+
+  // runner dependencies
+  val sprayCan         = "io.spray" %% "spray-can" % sprayVersion withSources() withJavadoc()
+  val sprayHttp        = "io.spray" %% "spray-http" % sprayVersion withSources() withJavadoc()
+  val sprayRouting     = "io.spray" %% "spray-routing" % sprayVersion withSources() withJavadoc()
+  val sprayTestkit     = "io.spray" %% "spray-testkit" % sprayVersion % "test" withSources() withJavadoc()
+
+  val specs2           = "org.specs2" %% "specs2-core" % "2.3.11" % "test"
+
+
+  val scalaXml         = "org.scala-lang.modules" %% "scala-xml" % "1.0.1" withSources() withJavadoc()
+
+
+
 
   // RBX dependencies
-  libraryDependencies += "com.lmax" % "disruptor" % disruptorVersion withSources() withJavadoc()
+  // libraryDependencies += "com.lmax" % "disruptor" % disruptorVersion withSources() withJavadoc()
 
   // Akka Streams a.k.a reactive streams
   // val rxstreams = "com.typesafe.akka" % "akka-stream-experimental_2.11" % "0.3"
@@ -118,6 +134,16 @@ object MyBuild extends Build {
   //
   // ++ Seq( run <<= run in Compile in core )
   // aggregate(macros, core)
+
+  // import spray.revolver.RevolverPlugin._
+
+  lazy val runner = Project (
+    "runner",
+    file("runner"),
+    settings = buildSettings ++ Revolver.settings ++ Seq(
+      libraryDependencies ++= Seq(sprayCan, sprayHttp, sprayRouting, akkaActor, scalaXml, sprayTestkit, specs2)
+    )
+  )
 
 
   /**
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -2,3 +2,4 @@ addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.5.2")
 
 addSbtPlugin("com.typesafe.sbt" % "sbt-multi-jvm" % "0.3.8")
 
+addSbtPlugin("io.spray" % "sbt-revolver" % "0.7.2")
