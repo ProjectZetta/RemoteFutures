5f5a266a1a940570c3e1aa34cb1d47bf40089671
Martin Senne <mail@martin-senne.de>
2014-09-11 22:40:39 +0200

    Stats added

--- runner/src/main/scala/org/remotefutures/runner/MyService.scala
+++ runner/src/main/scala/org/remotefutures/runner/MyService.scala
@@ -4,6 +4,23 @@ import akka.actor.Actor
 import spray.routing._
 import spray.http._
 import MediaTypes._
+import akka.actor.{Props, Actor}
+import akka.pattern.ask
+
+import scala.concurrent.duration._
+import akka.actor._
+import akka.pattern.ask
+import spray.routing.{HttpService, RequestContext}
+import spray.routing.directives.CachingDirectives
+import spray.can.server.Stats
+import spray.can.Http
+import spray.httpx.marshalling.Marshaller
+import spray.httpx.encoding.Gzip
+import spray.util._
+import spray.http._
+import MediaTypes._
+import CachingDirectives._
+
 // we don't implement our route structure directly in the service actor because
 // we want to be able to test it independently, without having to spin up an actor
 class MyServiceActor extends Actor with MyService {
@@ -17,6 +34,11 @@ class MyServiceActor extends Actor with MyService {
 }
 // this trait defines our service behavior independently from the service actor
 trait MyService extends HttpService {
+
+
+  // we use the enclosing ActorContext's or ActorSystem's dispatcher for our Futures and Scheduler
+  implicit def executionContext = actorRefFactory.dispatcher
+
   val myRoute2 =
     path("") {
       get {
@@ -32,8 +54,17 @@ trait MyService extends HttpService {
       }
     }
 
-  val myRoute: Route =
-    path("") {
+  val myRoute =
+    path("stats") {
+      get {
+        complete {
+          actorRefFactory.actorFor("/user/IO-HTTP/listener-0")
+            .ask(Http.GetStats)(1.second)
+            .mapTo[Stats]
+        }
+      }
+    }
+    path("command") {
       post {
         respondWithMediaType(`text/html`) { // XML is marshalled to `text/xml` by default, so we simply override here
           complete {
@@ -46,6 +77,18 @@ trait MyService extends HttpService {
         }
       }
     }
+
+  implicit val statsMarshaller: Marshaller[Stats] =
+  Marshaller.delegate[Stats, String](ContentTypes.`text/plain`) { stats =>
+    "Uptime : " + stats.uptime.formatHMS + '\n' +
+      "Total requests : " + stats.totalRequests + '\n' +
+      "Open requests : " + stats.openRequests + '\n' +
+      "Max open requests : " + stats.maxOpenRequests + '\n' +
+      "Total connections : " + stats.totalConnections + '\n' +
+      "Open connections : " + stats.openConnections + '\n' +
+      "Max open connections : " + stats.maxOpenConnections + '\n' +
+      "Requests timed out : " + stats.requestTimeouts + '\n'
+  }
 
   val test = path("") { complete { "a" }  }
 
