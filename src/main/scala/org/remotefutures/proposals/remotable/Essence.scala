2357a103d04662c7400bf8f64db75bd203b9fb7d
Martin Senne <mail@martin-senne.de>
2014-12-17 16:33:39 +0100

    Added latest changes from spores. Added Essence for some prototyping.

---
+++ src/main/scala/org/remotefutures/proposals/remotable/Essence.scala
@@ -0,0 +1,128 @@
+package org.remotefutures.proposals.essence
+
+import org.remotefutures.core.{impl, RemoteExecutionContext}
+import org.remotefutures.spores._
+
+import scala.concurrent.{ExecutionContext, Promise, Future}
+import scala.util.{Success, Failure}
+
+/**
+ * Created by mse on 12.11.14.
+ */
+object Remote {
+
+  // () => Future[T] ......
+  // () => Spore[Future[T]] ....
+  // Spore[Future[T]] ...... () (with context) => Future[T]
+
+  def apply[T](r: NullarySpore[Future[T]]) : Future[T] = {
+    println("Before remote apply - could be transfer")
+    r.apply()
+  }
+
+  def apply[R, T](r: Spore[R, Future[T]]) : Future[T] = {
+    null
+  }
+
+//  def apply[T]( r: => Future[T]) : Future[T] = {
+//    null
+//  }
+}
+
+object A {
+  def foo(a:Int, b:Int):Int = {
+    a * b * 3
+  }
+}
+
+object Essence {
+//
+//  import scala.concurrent.ExecutionContext.Implicits.global
+//
+//  def bar1(): Unit = {
+//    val x = Future("asdf")
+//    val y = x.map(x => x.length) // executed locally
+//  }
+//
+//  def r(y: Int) : (String => Int ) = {
+//    x => x.length * y
+//  }
+//
+//  def bar_first(): Unit = {
+//    val x: Future[Int] = Remote {
+//      spore {
+//        () => {
+//            println("first __ inside")
+//            Future {
+//              "asdf"
+//            }
+//        }
+//      }
+//    }.map(x => x.length)
+//
+//    x.onComplete {
+//      case Success(x) => { println(x)}
+//      case Failure(x) => { println("error in future")}
+//    }
+//  }
+
+  def example() : Unit = {
+//    val r: Spore[String, String] =
+//      spore {
+//        x => capture(x).replace('a', 'b')
+//      }
+
+//      val a: Spore[String, Int] = spore {
+//        (x:String) => x.length
+//      }
+
+  }
+
+  def examples2() : Unit = {
+    val s = spore {
+      (x: Int) => {
+        x
+      }
+    }
+  }
+
+  def examples3() : Unit = {
+    val s = spore {
+      val y = 3
+      (x: Int) => {
+        // x * y
+        A.foo(x, y)
+      }
+    }
+  }
+
+  // all objects, that are referenced within a spore need to be declared
+  // either as val or as parameter
+
+
+//  def bar_second(): Unit = {
+//    val x: Future[Int] = Remote {
+//      spore {
+//        () => {
+//          Future {
+//            println("second __ inside")
+//            "asdf"
+//          }.map( x => capture(x).length )
+//        }
+//      }
+//    }
+//
+//    x.onComplete {
+//      case Success(x) => { println(x)}
+//      case Failure(x) => { println("error in future")}
+//    }
+//  }
+//
+//  def main(args:Array[String]) : Unit = {
+//    println("First")
+//    bar_first()
+//
+//    println("Second")
+//    // bar_second()
+//  }
+}
--- src/main/scala/org/remotefutures/proposals/remotable/Remotable.scala
+++ src/main/scala/org/remotefutures/proposals/remotable/Remotable.scala
@@ -1,178 +1,177 @@
-package org.remotefutures.proposals.remotable
-
-import org.remotefutures.core.{impl, RemoteExecutionContext}
-import org.remotefutures.spores._
-
-import scala.concurrent.{ExecutionContext, Promise, Future}
-
-/**
- * Created by mse on 12.11.14.
- */
-object Remote {
-  // type Remotable[T] = Latent[NullarySpore[T]]
-
-  def apply[T](r: Latent[NullarySpore[T]]) : Future[T] = {
-    null
-  }
-
-  def apply[T](r: Future[NullarySpore[T]]) : Future[T] = {
-    null
-  }
-
-  def apply[T](spore: NullarySpore[T]): Future[T] = ???
-}
-
-object Foo {
-
-  import scala.concurrent.ExecutionContext.Implicits.global
-
-  def bar1() : Unit = {
-    val x = Future( "asdf" )
-    val y = x.map(x => x.length) // executed locally
-  }
-
-  def bar2() : Unit = {
-    val x: Future[Int] = Remote {
-      Latent {
-        spore {
-          () => "asdf"
-        }
-      }
-    }.map( x => x.length )
-  }
-
-  def bar3() : Unit = {
-    val z = Latent {"asdf"}
-
-    val x = Remote {
-      val l = Latent {
-        spore {
-          () => "asdf"
-        }
-      }
-      // .map(x => x.length)
-      l
-    }
-  }
-
-
-object RemoteFuture2 {
-  /**
-   * execute body on remote node in sync
-   */
-   def apply[T]( body: => T ) : Future[T] = ???
-
-}
-
-object Remote2 {
-
-
-  /**
-   * execute body on remote node async
-   */
-  def apply[T]( async: => Future[T] ) : Future[T] = {
-    // transfer async
-    val p = Promise[T]()
-    async.onComplete {
-      x => p.complete(x)
-    }
-    p.future
-  }
-}
-
-object Foo2 {
-  def foo() : Unit = {
-    val r: Future[Int] = RemoteFuture2 {
-       Future {
-         "asdf"
-       }.map( x => x.length )
-    }
-
-  }
-  def foo2() : Unit = {
-    val r = RemoteFuture2 {
-      "asdf"
-    }.map( x => x.length )
-
-  }
-}
-
-
-
-
+//package org.remotefutures.proposals.remotable
 //
-//  Remote {
-//    Latent {"asdf"}.map( x => x.length)
+//import org.remotefutures.core.{impl, RemoteExecutionContext}
+//import org.remotefutures.spores._
+//
+//import scala.concurrent.{ExecutionContext, Promise, Future}
+//
+///**
+// * Created by mse on 12.11.14.
+// */
+//object Remote {
+//  // type Remotable[T] = Latent[NullarySpore[T]]
+//
+//  def apply[T](r: Latent[NullarySpore[T]]) : Future[T] = {
+//    null
 //  }
 //
-//  Remote { // RemoteFuture
-//    "asdf".map( x => x.length )
+//  def apply[T](r: Future[NullarySpore[T]]) : Future[T] = {
+//    null
 //  }
 //
-//  Remote {
-//
-//  }
-
-
-//  implicit def latentToFuture[T]( latent: Latent[NullarySpore[T]]) : Future[T] = {
-//    latent.future
-//  }
-
-//  implicit def sporeToRegular[T]( r : Latent[NullarySpore[T]] ) : T = ???
-
-}
-
-
-
-
-object Latent {
-  def apply[T](body: => T) = new Latent(body)
-
-  // def apply[T](spore: NullarySpore[T]) = new Latent(spore)
-  def apply[T](spore: NullarySpore[T]) = new Latent(spore)
-}
-
-class Latent[T](f: => T)  {
-  private val p = Promise[T]()
-
-  def trigger() {
-    p.success(f)
-  }
-
-  def future: Future[T] = p.future
-
-  def map[S](f: T => S)(implicit executor: ExecutionContext): Future[S] =  { // transform(f, identity)
-    val p = Promise[S]()
-    // onComplete { v => p complete (v map f) }
-    p.future
-  }
-}
-
-
-class Latent_orig[T](f: => T) {
-  private val p = Promise[T]()
-
-  def trigger() {
-    p.success(f)
-  }
-
-  def future: Future[T] = p.future
-}
-
-object Latent_orig {
-  def apply[T](f: => T) = new Latent_orig(f)
-}
-
-//
-//// ==============
-//object Remotable {
-//  def apply[T](spore: NullarySpore[T])
+//  def apply[T](spore: NullarySpore[T]): Future[T] = ???
 //}
 //
-//trait Location {
+//object Foo {
+//
+//  import scala.concurrent.ExecutionContext.Implicits.global
+//
+//  def bar1() : Unit = {
+//    val x = Future( "asdf" )
+//    val y = x.map(x => x.length) // executed locally
+//  }
+//
+//  def bar2() : Unit = {
+//    val x: Future[Int] = Remote {
+//      Latent {
+//        spore {
+//          () => "asdf"
+//        }
+//      }
+//    }.map( x => x.length )
+//  }
+//
+//  def bar3() : Unit = {
+//    val z = Latent {"asdf"}
+//
+//    val x = Remote {
+//      val l = Latent {
+//        spore {
+//          () => "asdf"
+//        }
+//      }
+//      // .map(x => x.length)
+//      l
+//    }
+//  }
+//
+//
+//object RemoteFuture2 {
+//  /**
+//   * execute body on remote node in sync
+//   */
+//   def apply[T]( body: => T ) : Future[T] = ???
 //
 //}
 //
-//object Local extends Location {
+//object Remote2 {
+//
+//
+//  /**
+//   * execute body on remote node async
+//   */
+//  def apply[T]( async: => Future[T] ) : Future[T] = {
+//    // transfer async
+//    val p = Promise[T]()
+//    async.onComplete {
+//      x => p.complete(x)
+//    }
+//    p.future
+//  }
+//}
+//
+//object Foo2 {
+//  def foo() : Unit = {
+//    val r: Future[Int] = RemoteFuture2 {
+//       // "asdf".map( x => x.length )
+//      5
+//    }
+//
+//  }
+//  def foo2() : Unit = {
+//    val r = RemoteFuture2 {
+//      "asdf"
+//    }.map( x => x.length )
+//
+//  }
+//}
+//
+//
+//
+//
+////
+////  Remote {
+////    Latent {"asdf"}.map( x => x.length)
+////  }
+////
+////  Remote { // RemoteFuture
+////    "asdf".map( x => x.length )
+////  }
+////
+////  Remote {
+////
+////  }
+//
+//
+////  implicit def latentToFuture[T]( latent: Latent[NullarySpore[T]]) : Future[T] = {
+////    latent.future
+////  }
+//
+////  implicit def sporeToRegular[T]( r : Latent[NullarySpore[T]] ) : T = ???
 //
 //}
+//
+//
+//
+//
+//object Latent {
+//  def apply[T](body: => T) = new Latent(body)
+//
+//  // def apply[T](spore: NullarySpore[T]) = new Latent(spore)
+//  def apply[T](spore: NullarySpore[T]) = new Latent(spore)
+//}
+//
+//class Latent[T](f: => T)  {
+//  private val p = Promise[T]()
+//
+//  def trigger() {
+//    p.success(f)
+//  }
+//
+//  def future: Future[T] = p.future
+//
+//  def map[S](f: T => S)(implicit executor: ExecutionContext): Future[S] =  { // transform(f, identity)
+//    val p = Promise[S]()
+//    // onComplete { v => p complete (v map f) }
+//    p.future
+//  }
+//}
+//
+//
+//class Latent_orig[T](f: => T) {
+//  private val p = Promise[T]()
+//
+//  def trigger() {
+//    p.success(f)
+//  }
+//
+//  def future: Future[T] = p.future
+//}
+//
+//object Latent_orig {
+//  def apply[T](f: => T) = new Latent_orig(f)
+//}
+//
+////
+////// ==============
+////object Remotable {
+////  def apply[T](spore: NullarySpore[T])
+////}
+////
+////trait Location {
+////
+////}
+////
+////object Local extends Location {
+////
+////}
